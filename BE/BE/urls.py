77e1dfa5b7c6975d6c4d455d287c48a6ff49d751
ChoiCharles <ckstjr3592@naver.com>
2023-05-17 17:51:17 +0900

    ㅠㅠ

--- BE/BE/urls.py
+++ BE/BE/urls.py
@@ -1,4 +1,4 @@
-"""BE URL Configuration
+"""be URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('movies.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
 ]
